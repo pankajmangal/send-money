5f22520a9ca4815ba343b09e6e2a9c589e31eb78
Pankaj Mangal <pankajmangal@Pankajs-MacBook-Pro.local>
2024-12-19 15:15:02 +0530

    network base url has updated

--- lib/data/network/remote/api_endpoints.dart
+++ lib/data/network/remote/api_endpoints.dart
@@ -9,7 +9,8 @@ class ApiEndpoints {
   ApiEndpoints._internal();
 
   //Base url here....
-  static const baseUrl = "http://10.0.2.2:3000/";
+  // static const baseUrl = "http://10.0.2.2:3000/";
+  static const baseUrl = "https://b767-2401-4900-5fbf-eec-44bc-2b06-7724-eea3.ngrok-free.app/";
 
   //Api endpoints here...
   static const loginUrl = "api/authenticate";
